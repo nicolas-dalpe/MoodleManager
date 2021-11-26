9496a57fac22227a240f8050551e4783a47d268b
ndalpe <ndalpe@gmail.com>
2021-11-26 14:18:06 -0500

    Added settings in loop and confirm their value after update.

--- cfg.sh
+++ cfg.sh
@@ -1,58 +1,61 @@
-############################################################################
-# Those are the basic setting that should be run on a new Moodle instance. #
-############################################################################
+#############################################################################
+# Those are the basic settings that should be run on a new Moodle instance. #
+#############################################################################
 
-# Disable analytics model.
+# Declare the settings array
+# This array contains all the Moodle settings to be set
+# Look at the bottom of the file for plugin settings
+declare -A settings
 
 # Allow guest access to Dashboard
 # If enabled guests can access Dashboard, otherwise guests are
 # redirected to the site front page.
-php admin/cli/cfg.php --name=allowguestmymoodle --set=0
+settings["allowguestmymoodle"]=0
 
 # Self registration
 # If an authentication plugin, such as email-based self-registration,
 # is selected, then it enables potential users to register themselves
 # and create accounts. This results in the possibility of spammers
 # creating accounts in order to use forum posts, blog entries etc. for spam.
-php admin/cli/cfg.php --name=registerauth --set=''
+settings["registerauth"]=''
 
 # Force users to log in
 # Normally, the front page of the site and the course listings (but not courses)
 # can be read by people without logging in to the site. If you want to force
 # people to log in before they do ANYTHING on the site,
 # then you should enable this setting.
-php admin/cli/cfg.php --name=forcelogin --set=1
+settings["forcelogin"]=1
 
 # Default country
 # If you set a country here, then this country will be selected by default
 # on new user accounts. To force users to choose a country, just leave this unset.
-php admin/cli/cfg.php --name=country --set=CA
+settings["country"]='CA'
 
 # Default city
 # A city entered here will be the default city when creating new user accounts.
-php admin/cli/cfg.php --name=defaultcity --set=Montreal
+settings["defaultcity"]='Montreal'
 
 # Default timezone
 # This is the default timezone for displaying dates - each user can override
 # this setting in their profile. Cron tasks and other server settings are
 # specified in this timezone. You should change the setting if it shows
 # as "Invalid timezone"
-php admin/cli/cfg.php --name=timezone --set=America/Toronto
+settings["timezone"]='America/Toronto'
 
 # Guest Front page
 # The items selected above will be displayed on the site's front page.
-php admin/cli/cfg.php --name=frontpage --set=none
+settings["frontpage"]='none'
 
 # Guest login button
 # You can hide or show the guest login button on the login page.
-php admin/cli/cfg.php --name=guestloginbutton --set=0
+settings["guestloginbutton"]=0
 
 # Open to search engines
 # If you enable this setting, then search engines will be allowed to enter
 # your site as a guest. In addition, people coming in to your site via a
 # search engine will automatically be logged in as a guest. Note that this
 # only provides transparent access to courses that already allow guest access.
-php admin/cli/cfg.php --name=opentowebcrawlers --set=0
+settings["opentowebcrawlers"]=0
 
 # Allow indexing by search engines
 # This determines whether to allow search engines to index your site.
@@ -63,21 +66,29 @@ php admin/cli/cfg.php --name=opentowebcrawlers --set=0
 # "Nowhere" will tell search engines not to index any page. Note this
 # is only a tag in the header of the site. It is up to the search
 # engine to respect the tag.
-php admin/cli/cfg.php --name=allowindexing --set=2
+settings["allowindexing"]=2
 
 # Cookie prefix sessioncookie
 # This setting customises the name of the cookie used for Moodle sessions.
 # This is optional, and only useful to avoid cookies being confused when
 # there is more than one copy of Moodle running within the same web site.
-php admin/cli/cfg.php --name=sessioncookie --set=moodleeconcom
+settings["sessioncookie"]='moodleeconcom'
 
 # Automatically check for available updates
 # If enabled, your site will automatically check for available updates for
 # both Moodle code and all additional plugins. If there is a new update available,
 # a notification will be sent to site admins.
-php admin/cli/cfg.php --name=updateautocheck --set=0
+settings["updateautocheck"]=0
 
+echo -e "\n"
 
+for setting in "${!settings[@]}"
+do
+    php admin/cli/cfg.php --name=$setting --set=${settings[$setting]}
+    echo -n "$setting is now set to: "
+    php admin/cli/cfg.php --name=$setting
+    echo -e "\n"
+done
 
 #####################################################
 ############   D E V   S E T T I N G S   ############
